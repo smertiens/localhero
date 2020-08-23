9cabc6af219b80c72efc707d21e7695ae4f70221
smertiens <sean.mertiens@gmail.com>
2020-08-23 21:51:32 +0200

    another test

--- test.py
+++ test.py
@@ -4,58 +4,61 @@ import subprocess, threading
 import threading
 from queue import Queue, Empty
 
-interupt = False
+class OutputCollector(threading.Thread):
+
+    stdout_q = Queue()
+    stdout = None
+
+    def __init__(self, stdout):
+        super().__init__()
+        self.stdout = stdout
+
+    def run(self):
+        while True:
+            if (line := self.stdout.readline()) != '':
+                self.stdout_q.put(line)
+                print(line)
+            else:
+                break
 
 class Test(threading.Thread):
 
+    _stop = False
+    proc = None
+
+    def stop(self):
+        print('Stopping')
+        self._stop = True
+
     def run(self):
 
-        def test(buf, q: Queue):
-            for line in buf:
-                print(line)
-                q.put(line)
-            buf.close()
-                
-        proc = subprocess.Popen(
-            args='python3 testproc.py',
-            #args='source /Users/mephisto/projects/staycool-backend/.venv/bin/activate && flask run --port 5050',   # Python doc recommends str arg if shell=True
+        _stop = False
+
+        self.proc = subprocess.Popen(
+            #args='python3 testproc.py',
+            args='source /Users/mephisto/projects/staycool-backend/.venv/bin/activate && flask run --port 5050',   # Python doc recommends str arg if shell=True
             shell=True,
-            #env={'FLASK_APP': 'sc_backend_app.py'},
-            #cwd='/Users/mephisto/projects/staycool-backend',
+            env={'FLASK_APP': 'sc_backend_app.py'},
+            cwd='/Users/mephisto/projects/staycool-backend',
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True               # stdout as text not as byte stream
         )
 
-        q = Queue()
-        t = threading.Thread(target=test, args=(proc.stdout, q))
-        t.daemon = True # thread dies with the program
-        t.start()
+        sto = OutputCollector(self.proc.stdout)
+        sto.start()
 
-
-        while (res := proc.poll()) is None:
-            global interupt
-
-            try:
-                print('>> %s' % q.get_nowait())
-            except Empty:
-                pass
-
-            if interupt:
-                interupt = False
-                print('terminating')
-                #proc.terminate()
-                proc.send_signal(signal.SIGINT)
-                #proc.send_signal(signal.SIGINT)
-                #time.sleep(1)
-                break
+        while (res := self.proc.poll()) is None:
+            #try: print(sto.stdout_q.get_nowait())
+            #except Empty: pass 
+            if (line := self.proc.stdout.readline()) is not None:
+                print(line)
 
         print('Finished process with %s' % res)
         
 t = Test()
-#t.daemon = True
 t.start()
 print('Starting')
 time.sleep(3)
-
-interupt = True
+t.proc.terminate()
+#t.stop()
