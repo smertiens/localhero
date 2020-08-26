9cbf4db7495978d7f9ce02182819449e55e11df9
smertiens <sean.mertiens@gmail.com>
2020-08-26 15:36:30 +0200

    edited readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Localhero
 
+Localhero is a small GUI-application that helps you manage local server instances from your flask and vue-cli apps.
+
+The project is currently in beta status and tested on OSX.
+
+## Features
+
+* Start, stop and monitor local server instances in one place
+* Automatically activate corresponding virtual envs
+* Expandable via plugin system
+* Easy configuration via YAML file
+
+## Example configuration
+
+``` yaml
+servers:
+
+  MyApp API (Dev):
+    type: flask
+    env:
+      FLASK_APP: server.py
+    dir: /projects/flask_app
+    venv: $dir/.venv
+
+  MyApp Frontend (Dev):
+    type: vue-cli
+    dir: /projects/my_vue_app
+```
+
+## Changes
+
+### 0.1.0b1
+
+* First beta version
--- setup.py
+++ setup.py
@@ -16,28 +16,27 @@ setuptools.setup(
     packages=setuptools.find_packages(basepath + 'src'),
     package_dir={'': basepath + 'src'},
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Beta',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
     url='https://github.com/smertiens/localhero',
     project_urls={
-        #'Documentation': 'https://docs.atraxi-flow.com/',
         'Github': 'https://github.com/smertiens/localhero',
     },
     keywords='server manager developer tools local development',
     license='AGPL-3.0',
     author='Sean Mertiens',
     author_email='sean@atraxi-flow.com',
-    description='The flexible Python workflow tool',
+    description='Manage your local server instances in one place',
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
     python_requires='>=3.5',
 
     package_data={
-        #'atraxiflow': ['creator/assets/*', 'base/assets/*', 'templates/*'],
+        'localhero': ['config/*', 'style/*', 'fonts/*'],
     },
 
     entry_points={
